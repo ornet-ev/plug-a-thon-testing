8fcfc2bc82932e3b61073c0565cfacd1bf865ffd
David Gregorczyk <david.gregorczyk@web.de>
2026-01-23 12:03:30 +0100

    fix: #18 adapt code to updated libs.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,9 @@
-import org.ornet.pat.localization.db.v1.LocalizationServiceHandler
-import org.ornet.pat.mdib.v2.PAT_MDIB_V2
-import org.somda.dsl.biceps.installPostProcessor
+import org.ornet.pat.mdib.v2.patMdibV2
 import org.somda.dsl.rendering.jaxb.renderTo
 import java.io.File
 
 fun main() {
     val outputDir = File("generated_mdibs")
-
-    installPostProcessor(LocalizationServiceHandler(outputDir))
-    listOf(
-        PAT_MDIB_V2
-    ).onEach {
-        it.renderTo(outputDir)
-    }
+    val mdib = patMdibV2(outputDir)
+    mdib.renderTo(outputDir)
 }
