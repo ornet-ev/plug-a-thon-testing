068d6b07e480a22424b08ec860ebf65d77aca331
David Gregorczyk <david.gregorczyk@web.de>
2026-05-05 16:48:24 +0200

    ci: fix zipping site.

--- .github/copy_artifacts.sh
+++ .github/copy_artifacts.sh
@@ -3,6 +3,3 @@ mkdir plug-a-thon-mdibs
 cp -a build/mdib/build/generated_mdibs/. plug-a-thon-mdibs/
 cp -a resources/test-sequence.json plug-a-thon-mdibs/
 
-mkdir plug-a-thon-mdibs/site
-cp -a site/. plug-a-thon-mdibs/site/
-
